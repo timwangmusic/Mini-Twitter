eceef8718cbfb3ce1d73648ece2101f2cb135106
Tim Wang <weihewang2012@gmail.com>
2020-05-17 16:38:35 -0700

    implement timeline via K路归并算法

--- tweet/timeline.go
+++ tweet/timeline.go
@@ -1,3 +1,27 @@
 package tweet
 
+type PriorityQueue []Tweet
 
+func (pq PriorityQueue) Len() int {
+	return len(pq)
+}
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].CreatedAt.Sub(pq[j].CreatedAt) >= 0
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue) Push(item interface{}) {
+	*pq = append(*pq, item.(Tweet))
+}
+
+func (pq *PriorityQueue) Pop() (item interface{}){
+	prev := *pq
+	n := len(prev)
+	res := prev[n-1]
+	*pq = prev[:n-1]
+	return res
+}
